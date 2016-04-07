aaf54c3395418f771a110d67aaff8caec6e22064
fjulia <ferran.julia@gmail.com>
2016-04-08 00:06:16 +0200

    added gult script

---
+++ src/scripts/popup.js
@@ -0,0 +1,40 @@
+
+function configure() {
+
+    //var botId = '186718542:AAE38mXwxSRPw95m89Vbx1b4NMvOkuU4GuQ';
+    //var groupName = "Ferran";
+    //var origin = "41.494440, 2.359283"
+    var botId = $("#bot_token").val();
+    var groupName = $("#bot_group").val();
+    var origin = $("#route_origin").val();
+    if (botId != '' && groupName != '') {
+        //TODO Add bot into group and send hello message
+        $(this).find("span").addClass("fa-spin");
+        $(this).html('<span class="fa fa-refresh fa-spin"></span>  ' + "comprovant");
+
+
+        chrome.extension.sendMessage({
+            msg: "createGroup",
+            bot_id: botId,
+            groupName: groupName,
+            origin: origin
+        });
+
+
+
+    }
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if ("configured_ok" == message.status) {
+        window.close();
+    }else if ("configured_failed" == message.status) {
+        window.close();
+        alert("La configuració ha fallat. Assegure't d'haver entrat les dades de configuració correctament!");
+    }
+});
+
+
+$(document).ready(function () {
+    (localStorage.telegram_bot_token && localStorage.telegram_group_id) ? (console.log("telegram_bot_token was found"), $(".manual").remove()) : localStorage.ready ? $("#getID").prop("disabled", !1) : $("#getID").click(configure)
+});
